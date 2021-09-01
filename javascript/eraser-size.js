125f1dc580e4071a0674161df84c94ffe76b8baa
Kwong Chin Fung John <johnjaller@hotmail.com>
2021-09-01 16:35:09 +0800

    Adjustment to line width and eraser

--- javascript/eraser-size.js
+++ javascript/eraser-size.js
@@ -1,6 +1,6 @@
 let eraserSize= $("#eraserSize").val()
 $(".eraserSize-text").html(`Current eraser size: ${eraserSize}`);
-  $("#eraserSize").on("change", function () {
+  $("#eraserSize").on("input", function () {
     eraserSize= $("#eraserSize").val()
 $(".eraserSize-text").html(`Current eraser size: ${eraserSize}`);
     
