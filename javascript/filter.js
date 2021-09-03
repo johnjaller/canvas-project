d9fcc8385666005f3b5bb6dd3fac40a658df47d0
Janet Soong <janetsoong0730@gmail.com>
2021-09-04 01:03:34 +0800

    UI readme

--- javascript/filter.js
+++ javascript/filter.js
@@ -17,11 +17,34 @@ $('#brightness-range').change(() => {
     
 
 function filter() {
-    contextReal.clearRect(0,0,canvasReal.width,canvasReal.height)
-    contextReal.filter = `blur(${blur.value}px) grayscale(${grayscale.value}%) brightness(${brightness.value})`
-    let currentCanvas= new Image()
-        currentCanvas.src=canva
-        currentCanvas.onload=function(){contextReal.drawImage(currentCanvas,0,0)}
-console.log(contextReal.filter)
+    contextReal.filter = `blur(${blur.value}px) grayscale(${grayscale.value}%) brightness(${brightness.value}%)`
+console.log(brightness.value)
 }
+
+// var blur = document.getElementById('blur-range');
+// var brightness = document.getElementById('brightness-range');
+// var grayscale = document.getElementById('grayscale-range');
+
+
+// $('#blur-range').change(() => {
+//     filter();
+// });
+
+// $('#grayscale-range').change(() => {
+//     filter();
+// });
+
+// $('#brightness-range').change(() => {
+//     filter();
+// });
+    
+
+// function filter() {
+//     contextReal.clearRect(0,0,canvasReal.width,canvasReal.height)
+//     contextReal.filter = `blur(${blur.value}px) grayscale(${grayscale.value}%) brightness(${brightness.value})`
+//     let currentCanvas= new Image()
+//         currentCanvas.src=canva
+//         currentCanvas.onload=function(){contextReal.drawImage(currentCanvas,0,0)}
+// console.log(contextReal.filter)
+// }
 
