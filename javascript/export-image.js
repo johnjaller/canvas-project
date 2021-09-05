b7bdc9cf95915ca2dcc29fcaa79c1a9c24398a09
Kwong Chin Fung John <johnjaller@hotmail.com>
2021-09-05 11:21:40 +0800

    Bug fixed for image export

--- javascript/export-image.js
+++ javascript/export-image.js
@@ -1,4 +1,4 @@
-function exportImage()
+function exportImage(element)
 {
     
     element.href=canvasToImage("#ffffff")
@@ -6,41 +6,39 @@ function exportImage()
 
 function canvasToImage(backgroundColor){
 
-    var context = document.getElementById('canvas-real').getContext('2d');
-    
-    canvas = context.canvas;
+  
     //cache height and width        
-    var w = canvas.width;
-    var h = canvas.height;
+    var w = canvasReal.width;
+    var h = canvasReal.height;
     
     var data;
     
     //get the current ImageData for the canvas.
-    data = context.getImageData(0, 0, w, h);
+    data = contextReal.getImageData(0, 0, w, h);
     
     //store the current globalCompositeOperation
-    var compositeOperation = context.globalCompositeOperation;
+    var compositeOperation = contextReal.globalCompositeOperation;
     
     //set to draw behind current content
-    context.globalCompositeOperation = "destination-over";
+    contextReal.globalCompositeOperation = "destination-over";
     
     //set background color
-    context.fillStyle = backgroundColor;
+    contextReal.fillStyle = backgroundColor;
     
     //draw background / rect on entire canvas
-    context.fillRect(0,0,w,h);
+    contextReal.fillRect(0,0,w,h);
     
     //get the image data from the canvas
-    var imageData = this.canvas.toDataURL("image/jpeg");
+    var imageData = canvasReal.toDataURL("image/jpeg");
     
     //clear the canvas
-    context.clearRect (0,0,w,h);
+    contextReal.clearRect (0,0,w,h);
     
     //restore it with original / cached ImageData
-    context.putImageData(data, 0,0);
+    contextReal.putImageData(data, 0,0);
     
     //reset the globalCompositeOperation to what it was
-    context.globalCompositeOperation = compositeOperation;
+    contextReal.globalCompositeOperation = compositeOperation;
     
     //return the Base64 encoded data url string
     return imageData;
